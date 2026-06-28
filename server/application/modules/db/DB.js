90b120cca65c37ad6c280217f91d20bdd1710048
aleksandr813 <aleksandrtrushnikov18@rambler.ru>
2026-06-28 19:20:03 +0400

    new file:   server/application/modules/db/DB.js 	new file:   server/application/modules/db/ORM.js 	new file:   server/application/modules/db/data.db 	modified:   server/config.js 	modified:   server/package-lock.json 	modified:   server/package.json

---
+++ server/application/modules/db/DB.js
@@ -0,0 +1,15 @@
+const sqlite3 = require('sqlite3').verbose();
+const ORM = require('./ORM');
+
+
+class DB {
+    constructor({ DATABASE }) {
+        this.db = new sqlite3.Database(`${__dirname}/${DATABASE}`);
+        this.orm = new ORM(this.db);
+        this.initTables();
+    }
+
+    initTables() {
+        console.log("Создание/инициализация таблицы")
+    }
+}
--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     CORS: "*",
 
     URLS: {
-        MESSAGE: 'message',
+        MESSAGE: '/message',
     }
 }
 
