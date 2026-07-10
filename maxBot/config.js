906108074911797e3a5f64a0fefee944b57494ce
aleksandr813 <aleksandrtrushnikov18@rambler.ru>
2026-07-10 23:57:29 +0400

    modified:   maxBot/config.js

--- maxBot/config.js
+++ maxBot/config.js
@@ -1,5 +1,7 @@
 const CONFIG = {
     BOT_TOKEN: "f9LHodD0cOJvRoCDS0p8JCWd1CwmUZ_hnA44kRxKrBebn5i2EqBtMs8VqXT2koSXuzxL021wBj0iARSUZKps",
+    PORT: 3004,
+
     HOST: "localhost:3003",
     SERVER_TOKEN: "123123",
 
